a29d08d7bee4dbb08118e929f11ad725ee9a7561
ak-matsu <sandbox.matsu@gmail.com>
2021-03-25 21:53:20 +0900

    マウスオーバーすると色が変わる機能の設定

---
+++ app/javascript/articles.js
@@ -0,0 +1,8 @@
+// マウスオーバーすると色が変わる機能
+$(function () {
+  $(document).on("mouseover", "h1", function () {
+    $(this).css({ "color": "blue" });
+  }).on("mouseout", "h1", function () {
+    $(this).css({ "color": "" });
+  })
+});
