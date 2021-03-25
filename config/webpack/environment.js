84fbec7a13a2b2ff56fc7fcdf3d3c875897e260e
ak-matsu <sandbox.matsu@gmail.com>
2021-03-25 21:52:50 +0900

    jQuery設定と呼出

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+
+// 設定ファイルの書き換え、jQueryを管理下に追加
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    jquery: 'jquery',
+  })
+)
 
 module.exports = environment
